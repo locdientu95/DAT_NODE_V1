017cb0cfc4511e286b5ad78ff0e6f61be23a9e01
ntphu <ntphu240701@gmail.com>
2023-12-14 08:24:39 +0700

    PHU_DAT

--- route/authprocess.js
+++ route/authprocess.js
@@ -161,8 +161,10 @@ const getImage = (username) => {
 const deleteUser = (username) => {
   return new Promise(async (resolve, reject) => {
     try {
-      await MD.Register.findOneAndUpdate({ username: username })
+      console.log(username);
+      await MD.Register.findOneAndDelete({ username: username })
         .then((data) => {
+          console.log(data);
           resolve({ status: true, data: data });
         })
         .catch((err) => {
